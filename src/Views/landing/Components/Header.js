e1233b2a0899039b0d2fd238eca7a5239ef8e54d
belectrical <104729698+belectrical@users.noreply.github.com>
2022-05-03 21:48:09 +0200

    Update Header.js

--- src/Views/landing/Components/Header.js
+++ src/Views/landing/Components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Image, Box, Heading} from '@chakra-ui/core';
 
 const header = () => {
   return (
-  <>
-    <p> hola mundo </p>
-  </>
+    <Box> 
+      <Image src="./public/LOOGO_BELECTRICAL_NETWORK.png 
+         height={300}
+         width="100%"
+         objectFit = "cover"
+         alta="Beelectrical"
+        ></Image>
+    </Box>
   );
 };
-export default header
+export default header; 
