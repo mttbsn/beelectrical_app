8cbebd55bfc6e0be7b5e722d6ae02167eeea9373
belectrical <104729698+belectrical@users.noreply.github.com>
2022-05-04 22:01:34 +0200

    Update BoughtBTA.js

--- src/Views/landing/Components/BoughtBTA.js
+++ src/Views/landing/Components/BoughtBTA.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import {Heading} from '@chakra-ui/core';
+import {Heading,
+  InputGroup,
+  Box,
+  Input,
+  InputRightAddon,
+  InputRightElement,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  NumberInputStepper,
+  } from '@chakra-ui/core';
 
 const BoughtBTA = () => {
   return (
   <>
-    <Heading > decentrlaice the sum <Heading>
+    <Heading mt={20} textAlign= "centrer" as="h1" seze "2xl > decentrlaice the sum  </Heading>
+    <Heading textAling="centrer" as="h2" size ="xl" > BUY BEEL <Heading>
+    <Box as="form" mx ="auto" maxwidth={500} px= {10} > 
+      <InputGruop> 
+        <NumberInput width="100%" defaultValue={100} mt={10}>
+          <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+           </NumberInputStepper 
+         </NumberInput>
+      </InputGruop>
+     </Box>
   </>
   );
 };
